7b0f26975e15ac9fdf62b3dfe79a515814e8e027
mishalkamboh29 <164033516+mishalkamboh29@users.noreply.github.com>
2024-03-21 14:04:10 +0500

    this is zero array problem

---
+++ zero array.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main (){
+int arr[4]={0,1,0,2};
+int count=0;
+    for(int i=0;i<4;i++) {
+    if(arr[i]==0){
+               count++ ;
+               } 
+               }
+               cout<<"total zeros are"<<count<<endl; 
+               
+               
+               return 0;
