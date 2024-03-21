d5533fef75731cc6ba03842ef4e5907e1d7f215c
mishalkamboh29 <164033516+mishalkamboh29@users.noreply.github.com>
2024-03-21 13:28:28 +0500

    this is max array problem

---
+++ max array.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[4] ={ 5, 29, 11, 30};
+    
+    int max = 0;
+    
+    for(int i = 1; i<4 ; ++i) {
+        if(arr[i] > max) {
+            max = arr[i];
+        }
+    }
+    
+    cout << "the greatest element in the array is " << max << endl;
+
+    return 0;
+}
