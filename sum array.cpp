79d7013155aaef6d8861f46c74c62fb14b2a3476
mishalkamboh29 <164033516+mishalkamboh29@users.noreply.github.com>
2024-03-21 13:19:04 +0500

    this is sum problem

--- sum array.cpp
+++ sum array.cpp
@@ -7,16 +7,15 @@ int main() {
     int numbers[size];
     int sum = 0;
 
-    cout << "Enter 20 numbers: ";
+    cout << "enter 20 numbers: ";
     for (int i = 0; i < size; ++i) {
         cin >> numbers[i];
-    }
-
+}
     for (int i = 0; i < size; ++i) {
         sum += numbers[i];
     }
 
-    cout << "Sum of all elements in the array: " << sum << endl;
+    cout << "sum of all elements in the array: " << sum << endl;
 
     return 0;
 }
